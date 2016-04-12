6e1c984268d8f430c7fc25d6f665d7f5f7e5649f
Parker Smith <linuxlovr1@gmail.com>
2016-04-12 10:15:21 -0600

    added specs and stuff

--- spec/string_work_parker_spec.rb
+++ spec/string_work_parker_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
+
 describe StringWorkParker do
+  subject {StringWorkParker}
   it 'has a version number' do
     expect(StringWorkParker::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe 'manipulate strings' do 
+    let(:str) { 'My String' }
+
+    it 'reverses a string' do 
+      expect(subject.reversify(str)).to eq('gnirtS yM')
+    end
+
+    it 'adds spaces' do 
+      expect(subject.spacify(str, 1)).to eq('M y  S t r i n g')
+    end
   end
 end
